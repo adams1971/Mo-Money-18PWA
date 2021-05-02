3e4390583e8ffd5f1c6a21d0ff58ea9a3442a0da
adam stevenson <adamscdcm@gmail.com>
2021-05-02 10:00:55 -0700

    reconfig service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,71 +2,76 @@ const { response } = require("express");
 
 // based off unit 18 Ins-12, with reconfig files to cache
 const FILES_TO_CACHE = [
-    "/",
-    "/db.js",
-    "/index.html",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/style.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+  "/",
+  "/db.js",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/style.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const RUNTIME = "runtime";
 
 //installer
-self.addEventListener("install", function(evt) {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("files are sucessfully pre-cached!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-
-    self.skipWaiting()
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CASHE))
+      .then(self.skipWaiting())
+  );
 });
-//activator- activates the cache event and removes old data 
-self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("remove old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
 
-    self.Clients.claim();
+//activator- activates the cache event and removes old data
+self.addEventListener("activate", (event) => {
+  const currentCaches = [CACHE_NAME, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheNames) => !currentCaches.includes(cacheNames)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
 });
 
 //fetch
-self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                .then(response => {
-                    if (response.status === 200) {
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            }).catch(err => console.log(err))
-        );
-
-        return;
-    }
-    evt.respondWith(
-        caches.match(evt.request).then(function(response) {
-            return response || fetch(evt.request);
-        })
-    )
+self.addEventListener(`fetch`, (event) => {
+  if (event.request.url.includes(`/api/`)) {
+    event.respondWith(
+      caches
+        .open(RUNTIME)
+        .then((cache) =>
+          fetch(event.request)
+            .then((response) => {
+              //if valid response, clone and store to cache
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            //failed request- try to get from cache
+            .catch(() => cache.match(event.request))
+        )
+        .catch((err) => console.log(err))
+    );
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then((response) => response || fetch(event.request))
+    );
+  }
 });
